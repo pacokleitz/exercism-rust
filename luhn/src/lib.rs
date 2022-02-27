3eb78e00edf9d4d1adb452a6672f81b987face22
Paco <kpaco@pm.me>
2022-02-27 16:19:37 +0100

    19

---
+++ luhn/src/lib.rs
@@ -0,0 +1,13 @@
+/// Check a Luhn checksum.
+pub fn is_valid(code: &str) -> bool {
+    code.chars()
+        .filter(|c| !c.is_whitespace())
+        .rev()
+        .try_fold((0, 0), |(count, sum), val| {
+            val.to_digit(10)
+                .map(|num| if count % 2 == 1 { num * 2 } else { num })
+                .map(|num| if num > 9 { num - 9 } else { num })
+                .map(|num| (count + 1, num + sum))
+        })
+        .map_or(false, |(count, sum)| count > 1 && sum % 10 == 0)
+}
